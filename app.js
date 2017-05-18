5314d8cc0c52c06724079ef02212484235c92b8d
Bryan Hempen <brhempen@gmail.com>
2017-05-18 15:21:37 +0200

    only one person can talk at a time and only his audio is published on the session

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var apiKey = '45840352';
 var sessionId = '2_MX40NTg0MDM1Mn5-MTQ5NDY3OTM3NDcxNX54bVZGdnh5MDdqQ3BWQ1ZJMnRFcVREYU9-fg';
 var token = 'T1==cGFydG5lcl9pZD00NTg0MDM1MiZzaWc9MWZmM2MzYjAxMjYxMWQwNjkzYTE2NDgwZmQxODk0MjIyNjFlYWFjZjpzZXNzaW9uX2lkPTJfTVg0ME5UZzBNRE0xTW41LU1UUTVORFkzT1RNM05EY3hOWDU0YlZaR2RuaDVNRGRxUTNCV1ExWkpNblJGY1ZSRVlVOS1mZyZjcmVhdGVfdGltZT0xNDk0Njc5Mzk1Jm5vbmNlPTAuMTkwNTQ0NTQ4Nzg4OTk4OTYmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTQ5NzI3MTQwMQ==';
 
-var userId = "id" + Math.random().toString(16).slice(2);
+// stores the connectionId
+var connectionId;
+
+var allowedToTalk;
 
 
 $(document).ready(function () {
@@ -16,8 +19,6 @@ function logToConsole(s) {
     var timeStamp = timeNow.toLocaleTimeString();
     $("#console").prepend(timeStamp + " " + s + '<br>');
 }
-
-
 
 function initializeSession() {
     var session = OT.initSession(apiKey, sessionId);
@@ -80,10 +81,11 @@ function initializeSession() {
         */
     });
 
+    var publisher; 
     function initializePublisher(error) {
         // If the connection is successful, initialize a publisher and publish to the session
         if (!error) {
-            var publisher = OT.initPublisher('publisher', {
+            publisher = OT.initPublisher('publisher', {
                 insertMode: 'append',
                 width: '100%',
                 height: '100%'
@@ -93,7 +95,7 @@ function initializeSession() {
             publisher.on('audioLevelUpdated', function (event) {
                 var audioLevel = event.audioLevel;
                 if (audioLevel > 0.2) {
-                    logToConsole(" Currently talking. audioLevel " + event.audioLevel);
+                    //logToConsole(" Currently talking. audioLevel " + event.audioLevel);
                 }
             });
             session.publish(publisher);
@@ -102,34 +104,63 @@ function initializeSession() {
         }
     }
 
+    var connectionCount;
+    session.on({
+        connectionCreated: function (event) {
+            connectionCount++;
+            connectionId = session.connection.connectionId;
+            if (event.connection.connectionId != session.connection.connectionId) {
+                console.log('Another client connected. ' + connectionCount + ' total.');
+            }
+        },
+        connectionDestroyed: function connectionDestroyedHandler(event) {
+            connectionCount--;
+            console.log('A client disconnected. ' + connectionCount + ' total.');
+        }
+    });
+
     // Connect to the session
     session.connect(token, initializePublisher);
 
-    function requestToTalk() {
-        logToConsole("Request to Talk");
+    function requestToTalk(event) {
+        logToConsole("Event sent");
         session.signal(
             {
-                data: "RequestToTalk " + userId
-            },
-            function (error) {
-                if (error) {
-                    logToConsole("signal error ("
-                        + error.name
-                        + "): " + error.message);
-                } else {
-                    logToConsole("signal sent.");
-                }
+                data: "requestToTalk " + connectionId
             }
         );
     }
 
     $("#button1").click(requestToTalk);
 
-    session.on("signal", function (event) {        
-        if (!event.data.includes(userId)){
-            logToConsole("Signal sent from connection " + event.from.id);
-            logToConsole("Signal data " + event.data);
+    function enableTalking(){
+        logToConsole("enabling talking");
+        publisher.publishAudio(true);
+    }
+
+    function disableTalking(){
+        logToConsole("disabling talking");
+        publisher.publishAudio(false);
+    }
+
+    function receiveSignal(event) {
+        var res = event.data.split(" ");
+        var cmd = res[0];
+        var senderConnectionId = res[1];
+
+        switch (cmd) {
+            case "requestToTalk":
+                if (senderConnectionId != connectionId) {
+                    disableTalking();
+                } else {
+                    enableTalking();
+                }
+                break;
+            default:
+                logToConsole("ERROR: command not found");
         }
-    });
+    }
+
+    session.on("signal", receiveSignal);
 
 }
